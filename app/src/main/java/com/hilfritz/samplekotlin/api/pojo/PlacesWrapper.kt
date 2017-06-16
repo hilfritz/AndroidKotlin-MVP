50e2c8963853c66d3793ebf56f1389753493c179
Hilfritz Camallere <hilfritz@herdhr.com>
2017-06-16 15:53:30 +0800

    class update

--- app/src/main/java/com/hilfritz/samplekotlin/api/pojo/PlacesWrapper.kt
+++ app/src/main/java/com/hilfritz/samplekotlin/api/pojo/PlacesWrapper.kt
@@ -1,8 +1,21 @@
 package com.hilfritz.samplekotlin.api.pojo
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by Hilfritz Camallere on 16/6/17.
  *
  */
 class PlacesWrapper {
+
+    constructor() {}
+    constructor(place: List<PlaceItem>) {
+        this.place = place
+    }
+
+    @SerializedName("place_list")
+    @Expose
+    var place: List<PlaceItem>? = null
+
 }
