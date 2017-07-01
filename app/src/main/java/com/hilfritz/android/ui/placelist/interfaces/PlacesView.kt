61d1d8b9cb98979bbc86b4f2b0a5a66fe3efe2fe
Hilfritz <godofhandsomenessfritz@gmail.com>
2017-07-01 21:37:53 +0800

    removed the override in the PlacesView interface

--- app/src/main/java/com/hilfritz/android/ui/placelist/interfaces/PlacesView.kt
+++ app/src/main/java/com/hilfritz/android/ui/placelist/interfaces/PlacesView.kt
@@ -10,33 +10,6 @@ import java.util.*
  * Created by Hilfritz Camallere on 24/5/17.
  */
 interface PlacesView: BaseView {
-    override fun __findViews() {
-
-    }
-
-    override fun __showLoading() {
-
-    }
-
-    override fun __hideLoading() {
-
-    }
-
-    override fun __showFullScreenMessage(message: String) {
-
-    }
-
-    override fun __showDialog(tag: String, message: String) {
-
-    }
-
-    override fun __hideDialog(tag: String) {
-
-    }
-
-    override fun __initViews() {
-
-    }
 
     fun _showList()
 
